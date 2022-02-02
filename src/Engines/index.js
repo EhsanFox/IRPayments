73854ea58e8b2a849cb70255c13d33ec9519faa8
EhsanFox <EhsanFox@users.noreply.github.com>
2022-02-02 22:23:56 +0330

    🧹| Cleanup

--- src/Engines/index.js
+++
@@ -1,7 +0,0 @@
-import { IDPayEngine } from "./IDPay.js";
-import { ZarinpalEngine } from "./ZarinPal/index.js";
-
-export const Engines = { 
-    IDPay: IDPayEngine,
-    ZarinPal: ZarinpalEngine
-}
